c2746e767efe6a462a3f6589e9da2084a3d064ad
MD. Muhtasim Fuad <muhtasimfuad625@gmail.com>
2021-01-11 22:24:52 +0600

    Add files via upload

---
+++ Online Library/index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <title>Online Library</title>
+    <link rel="stylesheet" href="assets/vendor/font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" href="assets/vendor/animate.css/animate.css">
+    <link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="../assets/stylesheets/css/style.css">
+</head>
+<body>
+<br>
+<br>
+<br>
+
+<div class="container">
+    <div class="backIndex">
+        <div class="col-sm-4 col-sm-offset-4">
+            <a class="btn btn-primary btn-block" href="student/sign-in.php">Students</a>
+            <a class="btn btn-primary btn-block" href="librarian/admin-sign-in.php">Librarian</a>
+
+            </div>
+        </div>
+</div>
+
+<script src="../assets/vendor/jquery/jquery-1.12.3.min.js"></script>
+<script src="../assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="../assets/vendor/nano-scroller/nano-scroller.js"></script>
+<!--TEMPLATE scripts-->
+<!-- ========================================================= -->
+<script src="../assets/javascripts/template-script.min.js"></script>
+<script src="../assets/javascripts/template-init.min.js"></script>
+
+
+</body>
+</html>
